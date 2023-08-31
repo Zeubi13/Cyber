164eb2b5a7148982c5ef31cfe595db1b2b233ff4
PATHword <94932120+Zeubi13@users.noreply.github.com>
2023-08-31 08:30:14 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,2 @@
-<?php file_get_contents("http://challenge01.root-me.org/web-serveur/ch13/index.php"); ?>
+<?php $test = file_get_contents("http://challenge01.root-me.org/web-serveur/ch13/index.php"); 
+echo $test; ?>
